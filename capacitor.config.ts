123756baf949cfb8e96f69fe1eb7ed590a2af5ac
Akhdan Subagja <if23.akhdansubagja@mhs.ubpkarawang.ac.id>
2025-07-01 20:11:44 +0700

    Update image URLs to use production server and adjust environment configurations

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,23 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app', // ID Aplikasi Anda
-  appName: 'FinalProject',   // Nama Aplikasi Anda
-  webDir: 'www',             // Direktori aset web yang dikompilasi
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 0,     // Tampilkan splash screen selama 3 detik
-      launchAutoHide: false,        // PENTING: Jangan sembunyikan otomatis, kita akan sembunyikan di kode
-      backgroundColor: "#27ae60",   // Warna background splash screen (hijau GoRent All)
-      androidSplashResourceName: "splash", // Nama resource gambar splash di Android (sesuai yang digenerate Ionic)
-      androidScaleType: "CENTER_CROP", // Skala gambar splash
-      showSpinner: false,           // Jangan tampilkan spinner default
-      splashFullScreen: true,       // Splash screen mengambil layar penuh
-      splashImmersive: true,        // Mode immersive untuk Android
-      // iosSpinnerStyle: "small",    // Opsional: gaya spinner iOS jika showSpinner true
-      // spinnerColor: "#ffffff",     // Opsional: warna spinner jika showSpinner true
-    },
-  },
+  appId: 'io.ionic.starter',
+  appName: 'FinalProject',
+  webDir: 'www'
 };
 
 export default config;
