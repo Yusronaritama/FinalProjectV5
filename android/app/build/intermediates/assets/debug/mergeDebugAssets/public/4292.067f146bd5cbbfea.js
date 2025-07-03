c25a94df4fcc7c1fb5d4e46ad9f314007d63d547
Akhdan Subagja <if23.akhdansubagja@mhs.ubpkarawang.ac.id>
2025-07-04 02:23:15 +0700

    FIX: Clean git cache and enforce .gitignore rules

--- android/app/build/intermediates/assets/debug/mergeDebugAssets/public/4292.067f146bd5cbbfea.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[4292],{4292:(a,n,e)=>{e.r(n),e.d(n,{SplashScreenWeb:()=>o});var s=e(467),r=e(5083);class o extends r.E_{show(t){return(0,s.A)(function*(){})()}hide(t){return(0,s.A)(function*(){})()}}}}]);
