cfcc3ffc55d10b31299c9b0840cf703a58a046e7
Yusronaritama <if23.yusronaritama@mhs.ubpkarawang.ac.id>
2025-07-04 15:30:35 +0700

    apply gitignore rules locally

--- android/app/src/main/java/com/example/app/MainActivity.java
+++ android/app/src/main/java/com/example/app/MainActivity.java
@@ -1,5 +1,34 @@
-package com.example.app;
+import android.os.Bundle;
+import android.util.Log;
+import androidx.annotation.NonNull;
+import com.getcapacitor.BridgeActivity; // Pastikan ini sesuai dengan kelas induk Anda
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
 
-import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // Kode untuk mengambil token FCM secara manual
+    FirebaseMessaging.getInstance().getToken()
+      .addOnCompleteListener(new OnCompleteListener<String>() {
+        @Override
+        public void onComplete(@NonNull Task<String> task) {
+          if (!task.isSuccessful()) {
+            Log.w("FCM_TOKEN", "Fetching FCM registration token failed", task.getException());
+            return;
+          }
+
+          // Dapatkan token baru
+          String token = task.getResult();
+
+          // Log token
+          Log.d("FCM_TOKEN", "Manual fetch token: " + token);
+        }
+      });
+  }
+}
